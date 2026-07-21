59f9f75ad7b1a48bae852e9d890a0d3ed1d310b8
Kengo Suzuki <git@kengo.dev>
2026-07-21 21:30:57 +0900

    Add Host

---
+++ src/host.zig
@@ -0,0 +1,24 @@
+const Version = @import("version.zig").Version;
+
+pub const Host = extern struct {
+    clap_version: Version,
+    host_data: *anyopaque,
+    name: [*:0]const u8,
+    vendor: [*:0]const u8,
+    url: [*:0]const u8,
+    version: [*:0]const u8,
+    getExtension: *const fn (host: *const Host, extension_id: [*:0]const u8) callconv(.c) ?*const anyopaque,
+    requestRestart: *const fn (host: *const Host) callconv(.c) void,
+    requestProcess: *const fn (host: *const Host) callconv(.c) void,
+    requestCallback: *const fn (host: *const Host) callconv(.c) void,
+};
+
+comptime {
+    const raw = @import("raw.zig");
+    const abi = @import("abi.zig");
+    abi.assertStruct(Host, raw.clap_host_t);
+    abi.assertFnPtr(@FieldType(Host, "getExtension"), @FieldType(raw.clap_host_t, "get_extension"));
+    abi.assertFnPtr(@FieldType(Host, "requestRestart"), @FieldType(raw.clap_host_t, "request_restart"));
+    abi.assertFnPtr(@FieldType(Host, "requestProcess"), @FieldType(raw.clap_host_t, "request_process"));
+    abi.assertFnPtr(@FieldType(Host, "requestCallback"), @FieldType(raw.clap_host_t, "request_callback"));
+}
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,7 @@
 pub const version = @import("version.zig");
 pub const Version = version.Version;
 pub const plugin = @import("plugin.zig");
+pub const Host = @import("host.zig").Host;
 
 test {
     const std = @import("std");
