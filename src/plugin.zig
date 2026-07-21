a5706a26d25cba62c696883b279bbc88a39da65a
Kengo Suzuki <git@kengo.dev>
2026-07-21 20:09:46 +0900

    Add Descriptor

--- src/plugin.zig
+++ src/plugin.zig
@@ -1,4 +1,40 @@
 pub const Entry = @import("plugin/Entry.zig").Entry;
+const Version = @import("version.zig").Version;
+
+pub const Descriptor = extern struct {
+    clap_version: Version,
+    id: [*:0]const u8,
+    name: [*:0]const u8,
+    vendor: [*:0]const u8,
+    url: [*:0]const u8,
+    manual_url: [*:0]const u8,
+    support_url: [*:0]const u8,
+    version: [*:0]const u8,
+    description: [*:0]const u8,
+    features: [*:null]const ?[*:0]const u8,
+};
+
+comptime {
+    abi.assertStruct(Descriptor, raw.clap_plugin_descriptor_t);
+}
+
+pub const Descriptor = extern struct {
+    clap_version: Version,
+    id: [*:0]const u8,
+    name: [*:0]const u8,
+    vendor: [*:0]const u8,
+    url: [*:0]const u8,
+    manual_url: [*:0]const u8,
+    support_url: [*:0]const u8,
+    version: [*:0]const u8,
+    description: [*:0]const u8,
+    features: [*:null]const ?[*:0]const u8,
+};
+
+comptime {
+    abi.assertStruct(Descriptor, raw.clap_plugin_descriptor_t);
+}
+
 
 test {
     const std = @import("std");
