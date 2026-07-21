eca29dcdaeb0e37e6fb1bb4e9d58c96e06bf9603
Kengo Suzuki <git@kengo.dev>
2026-07-22 02:57:00 +0900

    Add Process

---
+++ src/process.zig
@@ -0,0 +1,31 @@
+const event = @import("event.zig");
+const Transport = event.Transport;
+const InputEvents = event.InputEvents;
+const OutputEvents = event.InputEvents;
+const AudioBuffer = @import("audio_buffer.zig");
+
+pub const Process = extern struct {
+    pub const Status = enum(i32) {
+        @"error" = 0,
+        @"continue" = 1,
+        continue_if_not_quiet = 2,
+        tail = 3,
+        SLEEP = 4,
+    };
+
+    steady_time: i64,
+    frames_count: u32,
+    transport: ?*const Transport,
+    audio_inputs: *const AudioBuffer,
+    audio_outputs: *AudioBuffer,
+    audio_inputs_count: u32,
+    audio_outputs_count: u32,
+    in_events: *const InputEvents,
+    out_events: *const OutputEvents,
+};
+
+comptime {
+    const raw = @import("raw.zig");
+    const abi = @import("abi.zig");
+    abi.assertStruct(Process, raw.clap_process_t);
+}
