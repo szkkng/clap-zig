5766f5a1f266476e45ac7425de6a193eeb05402e
Kengo Suzuki <git@kengo.dev>
2026-07-22 04:03:07 +0900

    Re-export core types from the root module

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,16 @@
 pub const version = @import("version.zig");
 pub const Version = version.Version;
+pub const event = @import("event.zig");
 pub const plugin = @import("plugin.zig");
 pub const Host = @import("host.zig").Host;
+pub const AudioBuffer = @import("audio_buffer.zig").AudioBuffer;
+pub const Process = @import("process.zig").Process;
+pub const ID = @import("id.zig").ID;
+pub const invalid_id = @import("id.zig").invalid_id;
+pub const Beattime = @import("fixedpoint.zig").Beattime;
+pub const Sectime = @import("fixedpoint.zig").Sectime;
+pub const beattime_factor = @import("fixedpoint.zig").beattime_factor;
+pub const sectime_factor = @import("fixedpoint.zig").sectime_factor;
 
 test {
     const std = @import("std");
