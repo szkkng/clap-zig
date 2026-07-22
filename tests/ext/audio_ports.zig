d6c0c8ba2449fbcc9af5d229b2389e9b66a54748
Kengo Suzuki <git@kengo.dev>
2026-07-23 00:37:11 +0900

    Add flags test

--- tests/ext/audio_ports.zig
+++ tests/ext/audio_ports.zig
@@ -7,3 +7,28 @@ test "audio port identifiers" {
     try testing.expectEqualStrings(clap.port_mono, raw.CLAP_PORT_MONO[0..]);
     try testing.expectEqualStrings(clap.port_stereo, raw.CLAP_PORT_STEREO[0..]);
 }
+
+test "audio port info flags" {
+    const Flags = clap.AudioPortInfo.Flags;
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORT_IS_MAIN, Flags{ .is_main = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORT_SUPPORTS_64BITS, Flags{ .supports_64bits = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORT_PREFERS_64BITS, Flags{ .prefers_64bits = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORT_REQUIRES_COMMON_SAMPLE_SIZE, Flags{ .requires_common_sample_size = true });
+}
+
+test "audio port rescan flags" {
+    const Flags = clap.host.AudioPorts.RescanFlags;
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_NAMES, Flags{ .names = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_FLAGS, Flags{ .flags = false });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_CHANNEL_COUNT, Flags{ .channel_count = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_PORT_TYPE, Flags{ .port_type = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_IN_PLACE_PAIR, Flags{ .in_place_pair = true });
+    try expectFlag(Flags, raw.CLAP_AUDIO_PORTS_RESCAN_LIST, Flags{ .list = true });
+}
+
+fn expectFlag(comptime Flags: type, expected: anytype, actual: Flags) !void {
+    try testing.expectEqual(
+        @as(u32, @intCast(expected)),
+        @as(u32, @bitCast(actual)),
+    );
+}
