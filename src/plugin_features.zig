b235bb88df69928a546ef01efeea01b1f1e74060
Kengo Suzuki <git@kengo.dev>
2026-07-22 18:20:26 +0900

    Add plugin feature documentation

--- src/plugin_features.zig
+++ src/plugin_features.zig
@@ -1,9 +1,35 @@
+//! This file provides a set of standard plugin features meant to be used
+//! within clap.plugin.Descriptor.features.
+//!
+//! For practical reasons we'll avoid spaces and use `-` instead to facilitate
+//! scripts that generate the feature array.
+//!
+//! Non-standard features should be formatted as follow: "$namespace:$feature"
+
 pub const feature = struct {
+    /////////////////////
+    // Plugin category //
+    /////////////////////
+
+    /// Add this feature if your plugin can process note events and then produce audio
     pub const instrument = "instrument";
+
+    /// Add this feature if your plugin is an audio effect
     pub const audio_effect = "audio-effect";
+
+    /// Add this feature if your plugin is a note effect or a note generator/sequencer
     pub const note_effect = "note-effect";
+
+    /// Add this feature if your plugin converts audio to notes
     pub const note_detector = "note-detector";
+
+    /// Add this feature if your plugin is an analyzer
     pub const analyzer = "analyzer";
+
+    /////////////////////////
+    // Plugin sub-category //
+    /////////////////////////
+
     pub const synthesizer = "synthesizer";
     pub const sampler = "sampler";
     pub const drum = "drum";
@@ -30,10 +56,15 @@ pub const feature = struct {
     pub const glitch = "glitch";
     pub const utility = "utility";
     pub const pitch_correction = "pitch-correction";
-    pub const restoration = "restoration";
+    pub const restoration = "restoration"; // repair the sound
     pub const multi_effects = "multi-effects";
     pub const mixing = "mixing";
     pub const mastering = "mastering";
+
+    ////////////////////////
+    // Audio Capabilities //
+    ////////////////////////
+
     pub const mono = "mono";
     pub const stereo = "stereo";
     pub const surround = "surround";
